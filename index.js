92abd6acab0c466072ac8f6fe3a8c5b6eeb2610f
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-18 16:37:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+    function scuberGreetingForFeet(someValue) {
+      //this is where we can use conditionals given our argument, someValue
+      //don't forget to return whatever the result is!
+      let greeting;
+      if(someValue < 400){
+        greeting = "This one is on me!";
+      }
+      else if(someValue > 400){
+        greeting = "You have to pay!"
+      }
+      
+      return greeting;
+    }
+    
+        function ternaryCheckCity() {
+      //this is where we can use conditionals given our argument, someValue
+      //don't forget to return whatever the result is!
+    }
+    
+        function switchOnCharmFromTip() {
+      //this is where we can use conditionals given our argument, someValue
+      //don't forget to return whatever the result is!
+    }
